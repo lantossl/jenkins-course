512da8bace7615c05175e26ce1dbee640575b670
lantossl <lantos.sl@itvagyunk.info>
2020-05-12 14:32:22 +0200

    Update nodejs.groovy
    Customized git repo data

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/lantossl/hello-nodejs.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('lantossl')
+            node / gitConfigEmail('lantos.sl@itvagyunk.info')
         }
     }
     triggers {
